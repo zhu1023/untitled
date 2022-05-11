65aa9040d4ad1b64e5d65ed85b1f017455a71d07
zhu1023 <105272743+zhu1023@users.noreply.github.com>
2022-05-11 20:55:10 +0800

    Update gitTest.java

--- src/main/java/com/tsongtian/git/gitTest.java
+++ src/main/java/com/tsongtian/git/gitTest.java
@@ -8,6 +8,6 @@ public class gitTest {
         System.out.println("hello,git4");
         System.out.println("master");
         System.out.println("hot-fix");
-        System.out.println("github");
+        System.out.println("pull");
     }
 }
